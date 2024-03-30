f37f08f935414d52060d9ffcf0e42fd5d6632163
jxhnkndl <jkroyston@gmail.com>
2024-03-30 10:38:08 -0400

    render concerts on page

--- app/page.js
+++ app/page.js
@@ -1,9 +1,39 @@
-import Image from "next/image";
+import Image from 'next/image';
+import dayjs from 'dayjs';
 
-export default function Home() {
+import { getConcerts } from '@/sanity/utils';
+
+export default async function Home() {
+  const concerts = await getConcerts();
+
+  console.log(concerts);
+
   return (
-    <main className="text-6xl text-center mt-20">
-      <h1>Concerts</h1>
+    <main className='px-10 my-20'>
+      <h1 className='text-6xl text-center font-medium uppercase mb-10'>
+        Concerts
+      </h1>
+      <div className='max-w-[1240px] mx-auto grid lg:grid-cols-2 grid-cols-1 gap-8'>
+        {concerts.map((concert) => (
+          <div key={concert._id} className='bg-slate-50 relative'>
+            <div className='p-4 uppercase font-extrabold flex flex-col justify-center items-center bg-black text-slate-50 absolute top-0 right-0 w-[100px] z-50'>
+              {dayjs(concert.date).format('MMM D')}
+            </div>
+            <div className='h-[400px] relative overflow-hidden'>
+              <Image
+                src={concert.image}
+                alt={concert.altText}
+                fill
+                className='object-cover object-top'
+              />
+            </div>
+            <div className='p-6'>
+              <h2 className='text-3xl font-medium mb-1'>{concert.headliner}</h2>
+              <p className='text-lg'>{concert.openers}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
