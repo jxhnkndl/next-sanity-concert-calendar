9d874a6f97b6954e01a34d3cfb4ac3cc766946a1
jxhnkndl <jkroyston@gmail.com>
2024-03-30 09:49:49 -0400

    init sanity cms

---
+++ app/studio/[[...index]]/page.jsx
@@ -0,0 +1,9 @@
+'use client'
+
+// Embedded Sanity Studio page
+import { NextStudio } from 'next-sanity/studio'
+import config from '../../../sanity.config'
+
+export default function StudioPage() {
+  return <NextStudio config={config} />
+}
